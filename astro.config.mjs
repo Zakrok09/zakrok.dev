721abe08a9a6dbf21e303aaca088eb46d0b931f4
kpanayotov <k.panayotov@student.tudelft.nl>
2023-11-13 12:11:20 +0100

    chore: add svelte, tailwind, sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+import svelte from "@astrojs/svelte";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), svelte(), sitemap()],
+  output: "server",
+  adapter: vercel()
 });
