3bb8612c85804d5e184fb7341ae42e4569f0f3b2
kpanayotov <k.panayotov@student.tudelft.nl>
2023-11-13 16:32:05 +0100

    chore: add daisyui, sass, typography for tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,24 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {},	
 	},
-	plugins: [],
+	daisyui: {
+		themes: [
+			{
+				primary: {
+					"primary": "#1f2937",
+					"secondary": "#AB59F7",
+					"accent": "#f3f759",
+					"neutral": "#111827",
+					"base-100": "#f3f4f6",
+					"info": "#38bdf8",
+					"success": "#a3e635",
+					"warning": "#fb923c",
+					"error": "#ef4444",
+				},
+			},
+		],
+	},
+	plugins: [require("@tailwindcss/typography"), require("daisyui")],
 }
