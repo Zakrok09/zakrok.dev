aaaf06f9289bd199bda9a3f22ff421efce7702f8
Zakrok09 <eccyboo@protonmail.com>
2024-08-11 19:37:53 +0200

    fix: standardize spacing

--- src/components/Section.svelte
+++ src/components/Section.svelte
@@ -2,6 +2,6 @@
     export let className:string = '';
 </script>
 
-<section class="px-4 md:px-20 lg:px-40 xl:px-96 {className}">
+<section class="py-20 px-4 md:px-20 lg:px-40 xl:px-96 {className}">
     <slot />
 </section>
