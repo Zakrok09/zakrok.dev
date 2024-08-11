fa321842a283dbb7e74f96b15402fa3c9a5dcdd7
Zakrok09 <eccyboo@protonmail.com>
2024-08-11 10:40:44 +0200

    chore: link to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
-import netlify from "@astrojs/netlify";
 import svelte from "@astrojs/svelte";
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify(),
-  integrations: [svelte(), tailwind()]
+  integrations: [svelte(), tailwind()],
+  adapter: vercel()
 });
