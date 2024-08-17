d09734120532b649e493337a89cdbccec1f89451
Zakrok09 <eccyboo@protonmail.com>
2024-08-17 22:55:49 +0200

    chore: robots.txt and sitemap generator

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://zakrok.dev',
   output: "server",
-  integrations: [svelte(), tailwind()],
+  integrations: [svelte(), tailwind(), sitemap()],
   adapter: vercel()
 });
